d46113061c5fea3bac8f05e122715df69b831a89
ninja0911 <araisora30@gmail.com>
2020-09-22 09:47:12 +0800

    insert migration

---
+++ database/migrations/2020_09_22_014553_insert_active.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class InsertActive extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('leads', function (Blueprint $table) {
+
+            $table->boolean('active')->default(true);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
